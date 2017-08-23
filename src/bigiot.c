20ab11b665897e3c2de482824e6dfb14929785cc
Xianleewu <xianleewu@163.com>
2017-08-23 19:11:28 +0800

    add email info
    Signed-off-by: Xianleewu <xianleewu@163.com>

--- src/bigiot.c
+++ src/bigiot.c
@@ -1,5 +1,5 @@
 /* Copyright (C)
- * 2017 - Xianlee.Wu
+ * 2017 - Xianlee.Wu  xianleewu@163.com
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
